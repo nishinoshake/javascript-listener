cbb47173f2beb9f2b8b3e79627dd1705e23c22ea
noplan1989 <lawson.and.7.11@gmail.com>
2018-12-16 06:47:30 +0900

    フォント生成スクリプトのリファクタ

--- scripts/subset-japanese.js
+++ scripts/subset-japanese.js
@@ -1,13 +1,11 @@
 const jsdom = require('jsdom')
+const path = require('path')
 const fs = require('fs')
 const { JSDOM } = jsdom
 const { Nuxt, Builder } = require('nuxt')
 
-const routes = ['/', '/page/', '/mouse/', '/form/', '/media/', '/animation/', '/device/', '/about/', '/coding/']
-const subsetPath = './scripts/subset.txt'
-const japaneseRegex = /^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]$/
-
-const config = require('../nuxt.config.js')
+const configPath = path.resolve(__dirname, '../nuxt.config.js')
+const config = require(configPath)
 const nuxt = new Nuxt(config)
 
 const listen = async function() {
@@ -15,37 +13,37 @@ const listen = async function() {
   await nuxt.listen(4000, 'localhost')
 }
 
-const scrape = async function() {
-  let text = ''
+const scrape = async routes => {
+  const text = await Promise.all(
+    routes.map(async route => {
+      const { html } = await nuxt.renderRoute(route)
+      const dom = new JSDOM(html)
 
-  for (let route of routes) {
-    const { html } = await nuxt.renderRoute(route)
-    const dom = new JSDOM(html)
+      return dom.window.document.body.textContent
+    })
+  )
 
-    text += dom.window.document.body.textContent
-  }
-
-  return text
+  return text.join('')
 }
 
-const subsetJapanese = text =>
-  text
-    .split('')
-    .filter(str => japaneseRegex.test(str))
+const subset = (text, regex) =>
+  [...text]
+    .filter(str => regex.test(str))
     .filter((str, i, self) => self.indexOf(str) === i)
     .join('')
 
 const main = async function() {
+  const routes = ['/', '/page/', '/mouse/', '/form/', '/media/', '/animation/', '/device/', '/about/', '/coding/']
+  const subsetPath = path.resolve(__dirname, 'subset.txt')
+  const japaneseRegex = /^[\u3040-\u309f\u30a0-\u30ff\u3001-\u3007\u4e00-\u9fef\uf900-\ufaff]$/
+
   await listen()
-
-  const text = await scrape()
-
-  const japanese = subsetJapanese(text)
+  const text = await scrape(routes)
+  const japanese = subset(text, japaneseRegex)
 
   fs.writeFileSync(subsetPath, japanese)
 
   nuxt.close()
-
   process.exit(0)
 }
 
